fe0b60638fdd72e41a527ad51c54f4a81109d962
Hector Alvarado <hectoralexanderalvarado@gmail.com>
2023-05-19 11:51:43 -0600

    Se movio el material module a la carpeta shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms'
-import { MaterialModule } from './models/material.module';
+import { MaterialModule } from './shared/material.module';
 import { HttpClientModule } from '@angular/common/http';
 
 
